18fb5127441a0868a224d59d2f97fcbb41cfae63
Cesaraugusto916 <cesar916augusto@gmail.com>
2025-03-20 18:01:26 -0300

    files organization

---
+++ Análise de Dados/proj02/calculadora.sh
@@ -0,0 +1,10 @@
+# Defino o caminho do arquivo Python da calculadora.
+$caminho_do_script_python = "/home/cesar/PycharmProjects/CursoemVideo/EBAC/calculadora.py"
+
+Write-Host "Iniciando a calculadora..."
+
+# Chamo o Python para executar o script da calculadora. 
+python3 $caminho_do_script_python
+
+# Após a execução da calculadora, eu vou exibir uma mensagem informando que o script terminou.
+Write-Host "Calculadora finalizada."
